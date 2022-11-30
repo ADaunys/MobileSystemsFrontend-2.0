f00c3a1070ccff567ffb00e44d96a2366dc9b720
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-11-30 16:50:55 +0200

    Added navigation and views, Map view

--- app/src/main/java/com/example/mobile_systems_frontend_new/MainActivity.kt
+++ app/src/main/java/com/example/mobile_systems_frontend_new/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.mobile_systems_frontend_new
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mobile_systems_frontend_new.repository.Repository
+import android.text.method.ScrollingMovementMethod
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,22 +20,47 @@ class MainActivity : AppCompatActivity() {
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        /*
-        viewModel.getPost()
-        viewModel.myResponse.observe(this, Observer{ response ->
-            Log.d("Response", response.toString())
-            var helloTextView: TextView = findViewById(R.id.text_id)
-            helloTextView.setText(response.toString())
-        })*/
+
         viewModel.calculateLocation()
         viewModel.calculationResponse.observe(this, Observer{ response ->
-            Log.d("Response", response.toString())
             var helloTextView: TextView = findViewById(R.id.text_id)
             var finalMessage = ""
             for(item in response.responses) {
                 finalMessage += item + "\n"
             }
-            helloTextView.setText(finalMessage)
+            helloTextView.text = finalMessage
         })
+    }
+
+    fun gotoHome(view: View){
+        setContentView(R.layout.activity_main)
+    }
+
+    fun gotoMap(view: View){
+        setContentView(R.layout.map_view)
+
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
+        viewModel.getPost()
+        viewModel.myResponse.observe(this, Observer{ response ->
+            val mapText: TextView = findViewById(R.id.mapTextView)
+            var map = response.map
+            map = map.replace("1", "■")
+            map = map.replace("0", "□")
+            mapText.text = map
+            mapText.movementMethod = ScrollingMovementMethod()
+        })
+    }
+
+    fun gotoSignals(view: View){
+        setContentView(R.layout.signal_strength_view)
+    }
+
+    fun onClick(view: View) {
+        setContentView(R.layout.map_view)
+
+
     }
 }
